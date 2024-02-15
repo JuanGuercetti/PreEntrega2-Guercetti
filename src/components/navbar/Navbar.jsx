4a92c747ea8352a5bddee0d09c858e20ac345ebf
JuanGuercetti <juanangu66@gmail.com>
2024-02-14 21:24:15 -0300

    Inicio del proyecto

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return (
+        <>
+        <header>
+            <h4>Happets</h4>
+
+            <nav>
+                <a href="">Collares</a>
+                <a href="">Pretales</a>
+                <a href="">Chapas Identificatorias</a>
+            </nav>
+        </header>
+        </>
+    )
+}
+
+export default Navbar;
