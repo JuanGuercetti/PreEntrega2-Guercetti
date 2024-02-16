b9d490a18d2961f31849b7e303fb093c76752ee5
JuanGuercetti <juanangu66@gmail.com>
2024-02-16 09:32:31 -0300

    Agregado logo y nuevas secciones

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,22 +1,38 @@
 import './Navbar.css'
+import logoHappets from '../../assets/logo.jpg'
 
 const Navbar = () => {
     return (
         <>
         <nav className="navbar navbar-expand bg-success">
             {/* Sacar los colores de boots, hacerlos en el css */}
-            <a className="navbar-brand" href="#">Happets</a>
+            {/* <a className="navbar-brand" href="#">Happets</a> */}
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    <img src={logoHappets} alt="Logo" className="logo-img d-inline-block align-text-top"/>
+                    Happets
+                </a>
+            </div>
 
             <div className="collapse navbar-collapse">
-                <ul className="navbar-nav">
+                <ul className="navbar-nav nav-underline">
                     <li>
-                        <a href="">Collares</a>
+                        <a href="" className='nav-link'>Correas</a>
                     </li>
                     <li>
-                        <a href="">Pretales</a>
+                        <a href="" className='nav-link'>Collares</a>
+                        {/* Regulables y tradicionales
+                        Hacer menú desplegable */}
                     </li>
                     <li>
-                        <a href="">Chapas Identificatorias</a>
+                        <a href="" className='nav-link'>Pretales</a>
+                    </li>
+                    <li>
+                        <a href="" className='nav-link'>Chapas Identificatorias</a>
+                    </li>
+                    <li>
+                        <a href="" className='nav-link disabled'>Bandanas</a>
+                        {/* Proximamente */}
                     </li>
                 </ul>
             </div>
