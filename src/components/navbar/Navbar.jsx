21f249dac88688a3210b267506e821e6f08ab395
JuanGuercetti <juanangu66@gmail.com>
2024-02-23 12:05:08 -0300

    Correciones menores

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,7 +6,6 @@ const NavBar = () => {
     return (
         <>
         <nav className="navbar navbar-expand">
-            {/* Sacar los colores de boots, hacerlos en el css */}
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">
                     <img src={logoBlack} alt="Logo" className="logo-img"/>
@@ -17,7 +16,7 @@ const NavBar = () => {
                         <li><a href="" className='nav-link link-light'>Correas</a></li>
                         <li><a href="" className='nav-link link-light'>Collares</a></li>
                             {/* Regulables y tradicionales
-                            Hacer menú desplegable */}
+                                Hacer menú desplegable */}
                         <li><a href="" className='nav-link link-light'>Pretales</a></li>
                         <li><a href="" className='nav-link link-light'>Chapas Identificatorias</a></li>
                         <li><a href="" className='nav-link link-light disabled'>Bandanas</a></li>
